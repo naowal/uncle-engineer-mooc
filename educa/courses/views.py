b700e30b31ca460e1cda64d2cdf16ec432a56aee
naowal <way.serongang@gmail.com>
2019-05-18 07:54:32 +0700

    สร้าง formsets สำหรับ โมดูล course

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -4,6 +4,13 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+# ชุด import สำหรับสร้าง fromsets
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
+
+
+
 from .models import Course
 
 # สร้าง Mixins สำหรับ views class-based
@@ -40,3 +47,27 @@ class CourseDeleteView(PermissionRequiredMixin,OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')
     permission_required = 'courses.delete_course'
+
+#สร้าง fromset เพื่อสร้าง แก้ไข ลบ module
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course,data=data)
+
+    def dispatch(self,request,pk):
+        self.course = get_object_or_404(Course,id=pk,owner=request.user)
+        return super(CourseModuleUpdateView,self.dispatch(request, pk))
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,'formset': formset})
+    
