863b951be88a6a2b5608f5dfdc1fd79bac31479a
naowal <way.serongang@gmail.com>
2019-05-23 10:13:21 +0700

    All

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -4,14 +4,15 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# ชุด import สำหรับสร้าง fromsets
+# ชุด import สำหรับสร้าง fromsets Module
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from .forms import ModuleFormSet
 
-
-
-from .models import Course
+# ชุด import สำหรับเพิ่ม content ย่อยๆ ให้กับ Module
+from django.forms.models import modelform_factory
+from django.apps import apps
+from .models import Module,Content,Course
 
 # สร้าง Mixins สำหรับ views class-based
 class OwnerMixin(object):
@@ -71,3 +72,45 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             return redirect('manage_course_list')
         return self.render_to_response({'course': self.course,'formset': formset})
     
+
+#คลาสสำหรับการเพิ่มและแก้ไขฟอร์ม content
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+    
+    def get_model(self, model_name):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model(app_label='courses', model_name=model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner','order','created','updated'])
+        return Form(*args, **kwargs)
+
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(Module,id=module_id,
+        course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(self.model,id=id,owner=request.user)
+        return super(ContentCreateUpdateView,self).dispatch(request, module_id, model_name, id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form,'object': self.obj})
+
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model,instance=self.obj,data=request.POST,files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            if not id:
+                Content.objects.create(module=self.module,item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response({'form': form, 'object': self.obj})
+    
+
+
